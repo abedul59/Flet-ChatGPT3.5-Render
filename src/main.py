1bef2bb3b746655bbb0af7be3d891ebbf3d91f8f
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-06 15:59:15 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,42 @@
 import flet as ft
+import os, openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+conversation = []
+
+class ChatGPT:  
+    
+
+    def __init__(self):
+        self.messages = conversation
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
+
+
+
+    def get_response(self, user_input):
+        conversation.append({"role": "user", "content": user_input})
+        
+
+        response = openai.ChatCompletion.create(
+	            model=self.model,
+                messages = self.messages
+
+                )
+
+        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+        
+        print("AI回答內容：")        
+        print(response['choices'][0]['message']['content'].strip())
+
+
+        
+        return response['choices'][0]['message']['content'].strip()
+
+
+chatgpt = ChatGPT()
+
+
 
 
 def main(page: ft.Page):
@@ -8,8 +46,9 @@ def main(page: ft.Page):
             page.update()
         else:
             name = txt_name.value
+            ai_reply_response = chatgpt.get_response(name)
             page.clean()
-            page.add(ft.Text(f"Hello, {name}!"))
+            page.add(ft.Text(f"ChatGPT AI: {ai_reply_response}"))
 
     txt_name = ft.TextField(label="Your prompt")
 
