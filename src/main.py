a25dd711a34721375982aaf66bd6d0277ea80013
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-06 15:18:00 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -27,4 +27,5 @@ def main(page: Page):
     )
 
 #flet.app(target=main)
-flet.app(target=main, view=flet.WEB_BROWSER)
+#flet.app(target=main, view=flet.WEB_BROWSER) 
+flet.app(target=main, view=None, port=8502)
