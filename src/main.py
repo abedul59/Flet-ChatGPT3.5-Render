8443268101a8bb6bb3ca867d4a464865c499df4a
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-06 15:47:13 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,31 +1,19 @@
-import flet
-from flet import IconButton, Page, Row, TextField, icons
+import flet as ft
 
-def main(page: Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = "center"
 
-    txt_number = TextField(value="0", text_align="right", width=100)
+def main(page: ft.Page):
+    def btn_click(e):
+        if not txt_name.value:
+            txt_name.error_text = "Please enter your prompt"
+            page.update()
+        else:
+            name = txt_name.value
+            page.clean()
+            page.add(ft.Text(f"Hello, {name}!"))
 
-    def minus_click(e):
-        txt_number.value = int(txt_number.value) - 1
-        page.update()
+    txt_name = ft.TextField(label="Your prompt")
 
-    def plus_click(e):
-        txt_number.value = int(txt_number.value) + 1
-        page.update()
+    page.add(txt_name, ft.ElevatedButton("Generate!", on_click=btn_click))
 
-    page.add(
-        Row(
-            [
-                IconButton(icons.REMOVE, on_click=minus_click),
-                txt_number,
-                IconButton(icons.ADD, on_click=plus_click),
-            ],
-            alignment="center",
-        )
-    )
 
-#flet.app(target=main)
-#flet.app(target=main, view=flet.WEB_BROWSER) 
 flet.app(target=main, view=None, port=8502)
