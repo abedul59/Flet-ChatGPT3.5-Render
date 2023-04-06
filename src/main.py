0e6f59e553ef2a8103f5230902dd460359c5251d
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-06 19:27:21 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+'''
 import flet as ft
 import os, openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -54,5 +55,38 @@ def main(page: ft.Page):
 
     page.add(txt_name, ft.ElevatedButton("Generate!", on_click=btn_click))
 
-
 flet.app(target=main, view=None, port=8502)
+'''
+
+import flet
+from flet import IconButton, Page, Row, TextField, icons
+
+def main(page: Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = "center"
+
+    txt_number = TextField(value="0", text_align="right", width=100)
+
+    def minus_click(e):
+        txt_number.value = int(txt_number.value) - 1
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = int(txt_number.value) + 1
+        page.update()
+
+    page.add(
+        Row(
+            [
+                IconButton(icons.REMOVE, on_click=minus_click),
+                txt_number,
+                IconButton(icons.ADD, on_click=plus_click),
+            ],
+            alignment="center",
+        )
+    )
+
+#flet.app(target=main)
+#flet.app(target=main, view=flet.WEB_BROWSER) 
+flet.app(target=main, view=None, port=8502)
+
